c8e8b9bfafd6cbf81754e8b9c1730c9a4e10eade
Sanath <sanath@entrusttitle.com>
2023-07-14 09:04:34 +0530

    Initial commit

---
+++ app/src/main/java/com/sanathcoding/barcodeandqrcodescanner/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.sanathcoding.barcodeandqrcodescanner
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sanathcoding.barcodeandqrcodescanner.ui.theme.BarCodeAndQRCodeScannerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BarCodeAndQRCodeScannerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
